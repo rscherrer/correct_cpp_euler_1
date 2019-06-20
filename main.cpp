c1dbecd59b1bcc70e1b50752514df27a6d94e44f
rscherrer <raphael.scherrer@evobio.eu>
2019-06-20 12:08:16 +0200

    Documented main function and removed some comments

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 
-/// Show the answer to Project Euler, problem 1 (https://projecteuler.net/problem=1):
-///
-/// If we list all the natural numbers below 10 
-/// that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
-/// The sum of these multiples is 23.
-///
-/// Find the sum of all the multiples of 3 or 5 below 1000.
-int main() 
+
+/// Program to find the sum of all multiples of 3 or 5 below 1000
+int main()
 {
-  std::cout << "123456\n";
+    std::cout << "123456\n";
 }
