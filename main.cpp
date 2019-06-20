8ad6b430b0619b8c4dea0345a3c113d4d2ec6090
rscherrer <raphael.scherrer@evobio.eu>
2019-06-20 14:30:07 +0200

    Resolved stupid mistake

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
     std::vector<int> multiples;
 
     // Find all the multiples of 3 or 5 below 1000 and put them in a vector
-    for (int number = 1; number < 999; ++number) {
+    for (int number = 1; number < 1000; ++number) {
         if (number % 3 == 0 || number % 5 == 0) {
             multiples.push_back(number);
         }
