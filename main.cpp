ccaa4219f8cf2bcd46e687ef6c47b7a066cfb882
rscherrer <raphael.scherrer@evobio.eu>
2019-06-20 12:21:28 +0200

    Double vertical bar instead of single for simple OR

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
 
     // Find all the multiples of 3 or 5 below 1000 and put them in a vector
     for (size_t number = 1u; number < 999u; ++number) {
-        if (number % 3u == 0u | number % 5u == 0u) {
+        if (number % 3u == 0u || number % 5u == 0u) {
             multiples.push_back(number);
         }
     }
