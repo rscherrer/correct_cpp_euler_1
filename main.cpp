1740303d6c7c68d118956588a56b384d687882b6
rscherrer <raphael.scherrer@evobio.eu>
2019-06-20 14:59:38 +0200

    Trying to get max coverage

--- main.cpp
+++ main.cpp
@@ -2,18 +2,28 @@
 #include <vector>
 #include <numeric>
 
-/// Program to find the sum of all multiples of 3 or 5 below 1000
-int main()
-{
 
+/// Function to find all multiples of 3 or 5 below 1000
+std::vector<int> findMultiples()
+{
     std::vector<int> multiples;
 
-    // Find all the multiples of 3 or 5 below 1000 and put them in a vector
     for (int number = 1; number < 1000; ++number) {
         if (number % 3 == 0 || number % 5 == 0) {
             multiples.push_back(number);
         }
     }
+
+    return multiples;
+}
+
+
+/// Program to find the sum of all multiples of 3 or 5 below 1000
+int main()
+{
+
+    // Find all the multiples of 3 or 5 below 1000 and put them in a vector
+    std::vector<int> multiples = findMultiples();
 
     // Sum the multiples
     const int sumMultiples = std::accumulate(multiples.begin(), multiples.end(), 0);
